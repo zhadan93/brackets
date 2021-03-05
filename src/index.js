5e4dcc8ecdb1db299e5d6a98fc55cbeecc070c31
zhadan93 <zhadan.evgenya@yandex.ru>
2021-03-05 17:29:01 +0300

    feat: implement brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let mas = [];
+    let brackets = new Map(bracketsConfig);
+
+    for (let i = 0; i < str.length; i++) {
+
+        if (str[i] == brackets.get(mas[mas.length - 1])) {
+            mas.pop();
+        } else {
+            mas.push(str[i])
+        }
+
+    }
+
+    return mas.length !== 0 ? false : true;
 }
+
